b917d8e7492bf138606332282f9b02ab7aa06521
El Juli <mayorga.julian@gmail.com>
2013-11-18 16:36:00 -0300

    feat(main): cambiar mostrooooo por naveeeeeee

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,9 +152,9 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= yeoman.dist %>/scripts/{,*/}*.js',
-            '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= yeoman.dist %>/scripts/**/*.js',
+            '<%= yeoman.dist %>/styles/**/*.css',
+            '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
